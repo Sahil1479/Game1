016e1d5eba3f15cdf682185b059472e24a93d1c9
Ankur13997 <54276967+Ankur13997@users.noreply.github.com>
2019-10-24 18:32:28 +0530

    ADDED NEW FILE

---
+++ STONE PAPPER SCISSOR.py
@@ -0,0 +1,41 @@
+import random
+l=["STONE","PAPER","SCISSOR"]
+print("Welcome ")
+x = int(input("NO OF TURNS: "))
+print("CHOOSE STONE, PAPER, SCISSOR")
+
+u=0
+c=0
+i=1
+while  (i<=x):
+    print("User turn:")
+    y= input()
+    
+    print("Computer Choosen: ")
+    t=random.randrange(3)
+    print(l[t])
+    if y=="STONE" and (l[t]=="SCISSOR"):
+        u+=1
+    elif l[t]=="STONE" and ( y=="SCISSOR"):
+        c+=1
+    elif y=="SCISSOR" and l[t]=="PAPER" :
+        u+=1
+    elif l[t]=="SCISSOR" and y=="PAPER" :
+        c+=1
+    elif y=="PAPER" and l[t]=="STONE":
+        u+=1
+    elif l[t]=="PAPER" and y=="STONE":
+        c+=1
+    elif y==l[t]:
+        print("TRY AGAIN")
+    i+=1
+print("USER: ",u , "   COMPUTER:",c)
+if u>c:
+    print("YOU WIN")
+elif c>u :
+    print("YOU LOSS")
+else:
+    print("GAME TIE")
+
+
+
